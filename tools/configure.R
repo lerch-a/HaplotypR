4f87579dd5e75244c409ca5df631b6e9df6cb2d8
jemunro <jacob.e.munro@gmail.com>
2019-10-31 19:34:06 +1100

    refactor configure script

--- tools/configure.R
+++ tools/configure.R
@@ -7,68 +7,49 @@ if ((!dir.exists(vsearch_dir)) || (!dir.exists(swarm_dir))) {
 
   unlink(vsearch_dir, recursive = TRUE)
   unlink(swarm_dir, recursive = TRUE)
-
+  
   tmpdir <- 'tmp'
   dir.create(tmpdir, showWarnings = FALSE)
+  dir.create('inst', showWarnings = FALSE)
 
   platform <- Sys.info()[["sysname"]]
 
   if (grepl('windows', Sys.info()[["sysname"]], ignore.case = TRUE)) {
     # windows binaries
     vsearch_bin_url <- 'https://github.com/torognes/vsearch/releases/download/v2.14.1/vsearch-2.14.1-win-x86_64.zip'
-    vsearch_bin_zip <- file.path(tmpdir, basename(vsearch_bin_url))
-    download.file(url = vsearch_bin_url, destfile = vsearch_bin_zip, mode = 'wb')
-    unzip(vsearch_bin_zip, exdir = tmpdir)
-
     swarm_bin_url <- 'https://github.com/torognes/swarm/releases/download/v3.0.0/swarm-3.0.0-win-x86_64.zip'
-    swarm_bin_zip <- file.path(tmpdir, basename(swarm_bin_url))
-    download.file(url = swarm_bin_url, destfile = swarm_bin_zip, mode = 'wb')
-    unzip(swarm_bin_zip, exdir = tmpdir)
+    unwrap <- unzip
 
   } else if (grepl('linux', Sys.info()[["sysname"]], ignore.case = TRUE)) {
     # linux binaries
     vsearch_bin_url <- 'https://github.com/torognes/vsearch/releases/download/v2.14.1/vsearch-2.14.1-linux-x86_64.tar.gz'
-    vsearch_bin_tar <- file.path(tmpdir, basename(vsearch_bin_url))
-    download.file(url = vsearch_bin_url, destfile = vsearch_bin_tar, mode = 'wb')
-    untar(vsearch_bin_tar, exdir = tmpdir)
-
     swarm_bin_url <- 'https://github.com/torognes/swarm/releases/download/v3.0.0/swarm-3.0.0-linux-x86_64.tar.gz'
-    swarm_bin_tar <- file.path(tmpdir, basename(swarm_bin_url))
-    download.file(url = swarm_bin_url, destfile = swarm_bin_tar, mode = 'wb')
-    untar(swarm_bin_tar, exdir = tmpdir)
+    unwrap <- untar
 
   } else {
     # macos binaries
     vsearch_bin_url <- 'https://github.com/torognes/vsearch/releases/download/v2.14.1/vsearch-2.14.1-macos-x86_64.tar.gz'
-    vsearch_bin_tar <- file.path(tmpdir, basename(vsearch_bin_url))
-    download.file(url = vsearch_bin_url, destfile = vsearch_bin_tar, mode = 'wb')
-    untar(vsearch_bin_tar, exdir = tmpdir)
-
     swarm_bin_url <- 'https://github.com/torognes/swarm/releases/download/v3.0.0/swarm-3.0.0-macos-x86_64.tar.gz'
-    swarm_bin_tar <- file.path(tmpdir, basename(swarm_bin_url))
-    download.file(url = swarm_bin_url, destfile = swarm_bin_tar, mode = 'wb')
-    untar(swarm_bin_tar, exdir = tmpdir)
+    unwrap <- untar
 
   }
-
-  # move vsearch install to 'inst/vsearch'
-  if (!dir.exists('inst'))
-    dir.create('inst', showWarnings = FALSE)
-
-  # rename vsearch and include src
+  
+  # download vsearch and move binary to inst/vsearch/bin/vsearch(.exe)
+  vsearch_bundle <- file.path(tmpdir, basename(vsearch_bin_url))
+  download.file(url = vsearch_bin_url, destfile = vsearch_bundle, mode = 'wb')
+  unwrap(vsearch_bundle, exdir = tmpdir)
   invisible(file.rename(list.files(tmpdir, pattern = 'vsearch', full.names = TRUE)[1], vsearch_dir))
-  vsearch_src_url <- 'https://github.com/torognes/vsearch/archive/v2.14.1.tar.gz'
-  # download.file(url = vsearch_src_url, destfile = file.path(vsearch_dir, basename(vsearch_src_url)), mode = 'wb')
   if (grepl('windows', Sys.info()[["sysname"]], ignore.case = TRUE)) {
     dir.create(file.path(vsearch_dir, 'bin'))
     invisible(file.rename(file.path(vsearch_dir, 'vsearch.exe'),
                           file.path(vsearch_dir, 'bin', 'vsearch.exe')))
   }
-
-  # rename vsearch and include src
+  
+  # download swarm and move binary to inst/swarm/bin/swarm(.exe)
+  swarm_bundle  <- file.path(tmpdir, basename(swarm_bin_url))
+  download.file(url = swarm_bin_url, destfile = swarm_bundle, mode = 'wb')
+  unwrap(swarm_bundle, exdir = tmpdir)
   invisible(file.rename(list.files(tmpdir, pattern = 'swarm', full.names = TRUE)[1], swarm_dir))
-  swarm_src_url <- 'https://github.com/torognes/swarm/archive/v3.0.0.tar.gz'
-  # download.file(url = swarm_src_url, destfile = file.path(swarm_dir, basename(swarm_src_url)), mode = 'wb')
 
   # remove tmpdir
   unlink(tmpdir, recursive = TRUE)
