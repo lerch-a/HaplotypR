ee54806733f60dd75a138062d8d47d0d53ea42d8
jemunro <jacob.e.munro@gmail.com>
2019-10-31 18:40:33 +1100

    remove unlink inst from cleanup

--- tools/cleanup.R
+++ tools/cleanup.R
@@ -1,8 +1,13 @@
 
-if (dir.exists('inst')) {
-  unlink('inst', recursive = TRUE)
-}
 
-if (dir.exists('tmp')) {
+vsearch_dir <- file.path('inst', 'vsearch')
+if (dir.exists(vsearch_dir))
+  unlink(vsearch_dir, recursive = TRUE)
+
+swarm_dir <- file.path('inst', 'swarm')
+if (dir.exists(swarm_dir))
+  unlink(swarm_dir, recursive = TRUE)
+
+if (dir.exists('tmp')) 
   unlink('tmp', recursive = TRUE)
-}
+  
