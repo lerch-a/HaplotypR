10563c81b76d26b5a209860af8d34f778ed06a2c
Anita Lerch <alerch2@nd.edu>
2019-12-17 10:21:15 -0500

    Remove shQuote leading to an error. shQuote added int the Rvsearch:::.vsearchBin() call

--- R/filterHaplotype.R
+++ R/filterHaplotype.R
@@ -18,7 +18,7 @@ checkChimeras <- function(representativesFile, method="vsearch", progressReport=
   borderfile <- sub("_sort.fasta", "_bordchimera.fasta", sortfile)
   resfile <- sub("_sort.fasta", "_chimeraResults.txt", sortfile)
   # vsearch --uchime_denovo R-1_Rep1.representatives_nonchimeras.fasta --nonchimeras R-1_Rep1.representatives_nonchimeras2.fasta
-  syscall <- paste("--uchime_denovo ", shQuote(sortfile),"--mindiffs", 3, "--minh", 0.2, "--nonchimeras", shQuote(nonchimerafile), "--chimeras", shQuote(chimerafile), "--borderline", shQuote(borderfile), "--uchimeout", shQuote(resfile), sep=" ")
+  syscall <- paste("--uchime_denovo ", sortfile, "--mindiffs", 3, "--minh", 0.2, "--nonchimeras", nonchimerafile, "--chimeras", chimerafile, "--borderline", borderfile, "--uchimeout", resfile, sep=" ")
   
   # check and set progress report function
   if(!is.function(progressReport))
@@ -151,7 +151,7 @@ createHaplotypOverviewTable <- function(allHaplotypesFilenames, clusterFilenames
     overviewHap[names(idx),"indels"] <- !idx		
     sr1 <- sr1[idx]		
   }
-  
+
   # only SNP variation - Final Haplotypes
 	overviewHap$snps <- NA_character_
 	if(!is.null(snpSet)){
